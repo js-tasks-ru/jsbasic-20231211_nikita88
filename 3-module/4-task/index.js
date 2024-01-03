81b361f98acf94b22b152a86927895a75288cbf0
unknown <n.chernovol@vinteo.ru>
2024-01-03 23:55:22 +0300

    3-module/4-task completed

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
-  // ваш код...
+  let result = '';
+  users.forEach(user => {
+    if (user.age <= age) {
+      result += `${user.name}, ${user.balance}\n`;
+    }
+  });
+  return result.trim();
 }
