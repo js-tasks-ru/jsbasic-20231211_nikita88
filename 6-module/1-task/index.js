78bc027e7b0094c1f1be6f874789fa2e02658ba5
Nikita Chernovol <n.chernovol@vinteo.ru>
2024-01-11 12:17:32 +0300

    module-6/task-1 completed

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,43 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = this.createTable(rows);
+  }
+  
+  createTable(rows) {
+    const table = document.createElement('table');
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+    const headerRow = document.createElement('tr');
+    const headers = ['Имя', 'Возраст', 'Зарплата', 'Город', ''];
+
+    for (let header of headers) {
+      const th = document.createElement('th');
+      th.textContent = header;
+      headerRow.appendChild(th);
+    }
+    thead.appendChild(headerRow);
+
+    for (let row of rows) {
+      const tr = document.createElement('tr');
+      
+      for (let key in row) {
+        const td = document.createElement('td');
+        td.textContent = row[key];
+        tr.appendChild(td);
+      }
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'X';
+      deleteButton.addEventListener('click', () => tr.remove());
+      const td = document.createElement('td');
+      td.appendChild(deleteButton);
+      tr.appendChild(td);
+      tbody.appendChild(tr);
+    }
+    
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    return table;
   }
 }
